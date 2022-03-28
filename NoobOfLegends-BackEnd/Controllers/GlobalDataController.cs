0ef8ec78a1e9aef6a0627cde32538ad7a73e4e2b
Teegan Krieger <68746768+TeeganKrieger@users.noreply.github.com>
2022-03-28 16:50:13 -0400

    Created GlobalDataController
    * Created Template for global data controller

---
+++ NoobOfLegends-BackEnd/Controllers/GlobalDataController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoobOfLegends.Models.Services;
+
+namespace NoobOfLegends_BackEnd.Controllers
+{
+    public class GlobalDataController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public GlobalDataController(AppDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        [HttpPost("/api/globaldata/execute")]
+        public async Task<IActionResult> ExecuteAggregation()
+        {
+            //Trigger execution of the GlobalDataAggregation System
+
+            return Ok(); //If it works
+            return StatusCode(500); //If it fails
+        }
+
+        [HttpGet("/api/globaldata/get/{role}/{rank}/{division}")]
+        public async Task<IActionResult> GetGlobalData(string role, string rank, string division)
+        {
+            //Query the database for the specific LolGlobalAverage object
+            //Then calculate the averages and put them into a seperate return object.
+            //This can be an anonymous object or a type.
+
+            return Ok(/*Some Data Object*/); //If it works
+            return BadRequest(); //If it fails
+        }
+    }
+}
