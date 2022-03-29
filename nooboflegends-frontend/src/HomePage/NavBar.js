e2bb5c5682eacb4672a78f07ec6e3636c2501de5
Teegan Krieger <68746768+TeeganKrieger@users.noreply.github.com>
2022-03-28 20:17:54 -0400

    Home Page Integration minus functionality
    * Home Page has been ported to react
    * Page Navigation works. Search functionality is missing until backend is implemented.

---
+++ nooboflegends-frontend/src/HomePage/NavBar.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import './NavBar.css';
+import logo from '../Resources/MiscIcons/favicon-white-hat-128.png';
+
+/* Component that renders an entire user profile using various sub components */
+export default class NavBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            changePage: this.props.changePageFunc
+        };
+    }
+
+    doNothing() { }
+
+    handleReturnHome(event) {
+        event.preventDefault();
+        this.state.changePage("Home", null);
+    }
+
+    handleSearchSubmit(event) {
+        event.preventDefault();
+        //Make Request to backend for user. If User is found, redirect to other page
+        this.state.changePage("Profile", null);
+    }
+
+    render() {
+
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" onClick={this.handleReturnHome.bind(this)}>
+                    <img src={logo} height="40" alt="" style={{"marginLeft":"1.5rem"}}/>
+                </a>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" onClick={this.handleReturnHome.bind(this)}>Home</a>
+                        </li>
+                        <li className="nav-item px-10">
+                            <a className="nav-link" onClick={ this.doNothing() }>Compare</a>
+                        </li>
+                    </ul>
+                    <ul className="navbar-nav" style={{ "list style": "none", "marginLeft": "auto", "marginRight": "1.5rem" }}>
+                        <form className="form-inline" onSubmit={this.handleSearchSubmit.bind(this)}>
+                            <div className="input-group">
+                                <input className="form-control ml-sm-2" type="search" placeholder="Search for Player" aria-label="Search" />
+                                <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            </div>
+                        </form>
+                    </ul>
+                </div>
+            </nav >
+        );
+    }
+
+
+
+}
